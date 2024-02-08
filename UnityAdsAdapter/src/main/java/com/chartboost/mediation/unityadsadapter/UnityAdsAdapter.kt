252dd93eb0201eae86ccccb33f42df0c2a99b316
Ray Graham <rayg@bitwisedesign.com>
2024-02-08 08:58:16 -0800

    Added the `weakContinuationRef` to the full screen show

--- UnityAdsAdapter/src/main/java/com/chartboost/mediation/unityadsadapter/UnityAdsAdapter.kt
+++ UnityAdsAdapter/src/main/java/com/chartboost/mediation/unityadsadapter/UnityAdsAdapter.kt
@@ -404,8 +404,13 @@ class UnityAdsAdapter : PartnerAdapter {
 
         return suspendCancellableCoroutine { continuation ->
             fun resumeOnce(result: Result<PartnerAd>) {
-                if (continuation.isActive) {
-                    continuation.resume(result)
+                val weakContinuationRef = WeakReference(continuation)
+                weakContinuationRef.get()?.let {
+                    if (it.isActive) {
+                        it.resume(result)
+                    }
+                } ?: run {
+                    PartnerLogController.log(SHOW_FAILED, "Unable to resume continuation once. Continuation is null.")
                 }
             }
 
