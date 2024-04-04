743960a1d601461b30d8721f5854b540705b9724
Ray Graham <rayg@bitwisedesign.com>
2024-04-04 07:23:17 -0700

    [HB-7187] Update UnityAds adapter to perform `setUp` on `IO` instead of `Main` (#53)

--- UnityAdsAdapter/build.gradle.kts
+++ UnityAdsAdapter/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         minSdk = 21
         targetSdk = 33
         // If you touch the following line, don't forget to update scripts/get_rc_version.zsh
-        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "4.4.10.0.0"
+        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "4.4.10.0.1"
         buildConfigField("String", "CHARTBOOST_MEDIATION_UNITY_ADS_ADAPTER_VERSION", "\"${android.defaultConfig.versionName}\"")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
