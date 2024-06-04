00c2ffbde1cc6bef020ba809caf9976842324085
chauduyphanvu <vu.chau@chartboost.com>
2024-06-04 16:09:23 -0400

    API 34 (#69)

--- UnityAdsAdapter/build.gradle.kts
+++ UnityAdsAdapter/build.gradle.kts
@@ -36,11 +36,11 @@ repositories {
 
 android {
     namespace = "com.chartboost.mediation.unityadsadapter"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         // If you touch the following line, don't forget to update scripts/get_rc_version.zsh
         android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "5.4.12.0.0"
         buildConfigField("String", "CHARTBOOST_MEDIATION_UNITY_ADS_ADAPTER_VERSION", "\"${android.defaultConfig.versionName}\"")
